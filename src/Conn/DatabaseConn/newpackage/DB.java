d5c6e98489a012d1ce43382fa72068a830b2f79e
RAM <cristopherram55@gmail.com>
2026-03-25 22:50:34 +0800

    POS and DASHBOARD

---
+++ src/Conn/DatabaseConn/newpackage/DB.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conn.DatabaseConn.newpackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author RAM
+ */
+public class DB {
+
+    public static Connection con = null;
+
+    public static void loadConnection(String DBName, String username , String pass) {
+        String url = "jdbc:mysql://localhost:3306/" + DBName;
+        String user = username;
+        String password = pass;
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Database Error: " + e.getMessage());
+        }
+    }
+}
---
+++ src/Conn/DatabaseConn/newpackage/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conn.DatabaseConn.newpackage;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author RAM
+ */
+class DriverManager {
+
+    static Connection getConnection(String url, String root, String Password) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
